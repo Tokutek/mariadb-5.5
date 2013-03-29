20199f7d653c9d17efacfbbe684aa7c4e6a8575b
Rich Prohaska <prohaska@tokutek.com>
2013-03-29 13:21:35 +0000

    refs #6090 write new frm from alter table
    git-svn-id: file:///svn/mysql.com/mariadb-5.5.30@54894 c7de825b-a66e-492c-adef-691d508d4ae1

--- sql/handler.h
+++ sql/handler.h
@@ -2941,6 +2941,8 @@ public:
   { return 0; }
   virtual int prepare_range_scan(const key_range *start_key, const key_range *end_key)
   { return 0; }
+  virtual int new_alter_table_frm_data(const uchar *frm_data, size_t frm_len)
+  { return 0; }
 };
 
 #include "multi_range_read.h"
